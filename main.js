90451abb58b66365866aa6179024ac7203a99753
Emmanuel L. Nogueira <emmanuellacerd4@gmail.com>
2024-10-21 22:22:04 -0300

    mini dev

--- main.js
+++ main.js
@@ -1,28 +1,50 @@
-/**
- * 
- * @author Emmanuel L. Nogueira
- */
-
 console.log("Processo principal")
 
-const { app, BrowserWindow } = require('electron/main')
+// Importação de pacotes (bibliotecas)
+// nativeTheme (forçar um tema no sistema operacional)
+// Menu (criar um menu personalizado)
+// Shell (acessar links externos)
+const { app, BrowserWindow, nativeTheme, Menu, shell } = require('electron/main')
 const path = require('node:path')
 
+// Janela principal
+let win // Importante! Neste projeto o escopo da variavél win deve ser global
 function createWindow() {
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600,
+    nativeTheme.themeSource = 'dark' // Janela sempre escura
+    win = new BrowserWindow({
+        width: 1010, // Largura em pixels
+        height: 720, // Altura em pixels
         webPreferences: {
             preload: path.join(__dirname, 'preload.js')
         }
     })
+    // Menu personalizado
+    Menu.setApplicationMenu(Menu.buildFromTemplate(template))
 
-    win.loadFile('index.html')
+    win.loadFile('./src/views/index.html')
 }
 
+// Janela sobre
+function aboutWindow() {
+    nativeTheme.themeSource = 'dark'
+    const about = new BrowserWindow({
+        width: 360,
+        height: 220,
+        autoHideMenuBar: true, // Esconder o menu
+        resizable: false, // Impedir redimensionamento
+        minimizable: false, // Impedir minimizar a janela
+        //titleBarStyle: 'hidden' // Esconder a barra de estilo (ex: totem de auto atendimento)
+    })
+
+    about.loadFile('./src/views/sobre.html')
+
+}
+
+// Execução assincrona do aplicativo electron
 app.whenReady().then(() => {
     createWindow()
 
+    // Comportamento do MAC ao fechar uma janela
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) {
             createWindow()
@@ -30,8 +52,129 @@ app.whenReady().then(() => {
     })
 })
 
+// Encerrar a aplicação quando a janela for fechada (Windows e Linux)
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         app.quit()
     }
 })
+
+// Template do menu
+const template = [
+    {
+        label: 'Arquivo',
+        submenu: [
+            {
+                label: 'Novo',
+                accelerator: 'CmdOrCtrl+N'
+            },
+            {
+                label: 'Abrir',
+                accelerator: 'CmdOrCtrl+O'
+            },
+            {
+                label: 'Salvar',
+                accelerator: 'CmdOrCtrl+S'
+            },
+            {
+                label: 'Salvar Como',
+                accelerator: 'CmdOrCtrl+Shift+S'
+            },
+            {
+                type: 'separator'
+            },
+            {
+                label: 'Sair',
+                accelerator: 'Alt+F4',
+                click: () => app.quit()
+            }
+        ]
+    },
+    {
+        label: 'Editar',
+        submenu: [
+            {
+                label: 'Desfazer',
+                role: 'undo'
+            },
+            {
+                label: 'Refazer',
+                role: 'redo'
+            },
+            {
+                type: 'separator'
+            },
+            {
+                label: 'Recortar',
+                role: 'cut'
+            },
+            {
+                label: 'Copiar',
+                role: 'copy'
+            },
+            {
+                label: 'Colar',
+                role: 'paste'
+            },
+        ]
+    },
+    {
+        label: 'Zoom',
+        submenu: [
+            {
+                label: 'Aplicar zoom',
+                role: 'zoomIn'
+            },
+            {
+                label: 'Reduzir',
+                role: 'zoomOut'
+            },
+            {
+                label: 'Restaurar o zoom padrão',
+                role: 'resetZoom'
+            },
+        ]
+    },
+    {
+        label: 'Cor',
+        submenu: [
+            {
+                label: 'Amarelo'
+            },
+            {
+                label: 'Azul'
+            },
+            {
+                label: 'Laranja'
+            },
+            {
+                label: 'Pink'
+            },
+            {
+                label: 'Roxo'
+            },
+            {
+                label: 'Verde'
+            },
+            {
+                type: 'separator'
+            },
+            {
+                label: 'Restaurar a cor padrão'
+            },
+        ]
+    },
+    {
+        label: 'Ajuda',
+        submenu: [
+            {
+                label: 'Repositório',
+                click: () => shell.openExternal('https://github.com/emmanuel-lacerd4/minidev')
+            },
+            {
+                label: 'Sobre',
+                click: () => aboutWindow()
+            }
+        ]
+    }
+]
